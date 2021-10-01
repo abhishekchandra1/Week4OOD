81ce407e443affb15f423d303a4257a68883ed07
abhishekchandra1 <65722067+abhishekchandra1@users.noreply.github.com>
2021-10-01 14:43:39 -0400

    Add files via upload

---
+++ Iterator design Pattern on different types of balls/src/NameRepository.java
@@ -0,0 +1,31 @@
+public class NameRepository implements Container {
+    public String names[] = {"Tennis ball" , "Cricket ball" ,"Base ball" , "Football"};
+
+    @Override
+    public Iterator getIterator() {
+        return new NameIterator();
+    }
+
+    private class NameIterator implements Iterator {
+
+        int index;
+
+        @Override
+        public boolean hasNext() {
+
+            if(index < names.length){
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public Object next() {
+
+            if(this.hasNext()){
+                return names[index++];
+            }
+            return null;
+        }
+    }
+}
